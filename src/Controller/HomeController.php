a31262340c45f5ce68f125faed11028a0dab4e9c
MarcYoba <yobamarc1@gmail.com>
2026-09-12 13:57:57 +0100

    configuration du projet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home',methods: ['GET'])]
     public function index(): JsonResponse
     {
         return $this->json([
