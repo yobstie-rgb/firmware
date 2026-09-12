6f4afa25820017a05e0ed28da9eb63dbc2ebeb20
MarcYoba <yobamarc1@gmail.com>
2026-09-12 13:32:53 +0100

    modification de / dans le home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): JsonResponse
     {
         return $this->json([
